2670575c0c48f3f05e46f8cd5b46f82bcdc42090
Jake <jake@evansturner.co.uk>
2011-04-30 07:23:10 +0100

    start of output code WIP

--- gym_test.c
+++ gym_test.c
@@ -1,15 +1,24 @@
 #include "lj_gym.h"
 #include "lj_ym2612.h"
 
+#include <malloc.h>
+#include <stdio.h>
+
 int main(int argc, char* argv[])
 {
+	int result = LJ_GYM_OK;
+
 	LJ_GYM_FILE* gymFile = NULL;
 	LJ_GYM_INSTRUCTION gymInstruction;
 
 	LJ_YM2612* ym2612 = NULL;
 
-	int result = LJ_GYM_OK;
-	
+	LJ_YM_INT16* outputs[2];
+	outputs[0] = malloc(1024 * sizeof(LJ_YM_INT16));
+	outputs[1] = malloc(1024 * sizeof(LJ_YM_INT16));
+	int numCycles = 128;
+
+
 	gymFile = LJ_GYM_create( "test.gym" );
 	ym2612 = LJ_YM2612_create();
 
@@ -28,8 +37,13 @@ int main(int argc, char* argv[])
 				result = LJ_YM2612_setRegister(ym2612, 1, gymInstruction.R, gymInstruction.D);
 			}
 		}
+		if (result == LJ_GYM_OK)
+		{
+			result = LJ_YM2612_generateOutput(ym2612, numCycles, outputs);
+		}
 	};
 
 	LJ_GYM_destroy(gymFile);
+	LJ_YM2612_destroy(ym2612);
 	return -1;
 }
