49ed3da6b954f75c6ec132d2af0daca8656b617e
Jake <jake@evansturner.co.uk>
2011-06-03 19:32:32 +0100

    added test for start button to restart playing the test program (with debounce - restarts on start button release)

--- ASM/jakeTest.asm
+++ ASM/jakeTest.asm
@@ -1,3 +1,4 @@
+CPU=68000
 
 	dc.l $00000000,	EntryPoint,	ErrorTrap,	ErrorTrap	;00
 	dc.l ErrorTrap,	ErrorTrap,	ErrorTrap,	ErrorTrap	;10
@@ -16,17 +17,26 @@
 	dc.l ErrorTrap,	ErrorTrap,	ErrorTrap,	ErrorTrap	;E0
 	dc.l ErrorTrap,	ErrorTrap,	ErrorTrap,	ErrorTrap	;F0
 
+; D0 = spare temporary
+; D1 = spare temporary
+; D2 = spare temporary
+; D3 = used as loop counter inside output samples
+; D4 = used inside UpdateGUI
+; D5 = used inside UpdateGUI for joy pad reading
+; D6 = used inside UpdateGUI for joy pad reading
+; D7 = return value from UpdateGUI
+
 check_sum equ	$0000
 ;------------------------------------------------------------------------
 	dc.b	'SEGA GENESIS    '	;100
-	dc.b	'(C)T-xx 2008.06 '	;110 release year.month
+	dc.b	'(C)T-01 2011.06 '	;110 release year.month
 	dc.b	'JAKE YM2612 TEST'	;120 Japan title
 	dc.b	':Sample Doc Prog'	;130
 	dc.b	'                '	;140
 	dc.b	'JAKE YM2612 TEST'	;150 US title
 	dc.b	':Sample Doc Prog'	;160
 	dc.b	'                '	;170
-	dc.b	'GM T-XXXXXX XX'  	;180 product #, version
+	dc.b	'GM T-123456 42'  	;180 product #, version
 	dc.w	check_sum         	;18E check sum
 	dc.b	'J               '	;190 controller
 	dc.l	$00000000,$0007ffff,$00ff0000,$00ffffff		;1A0
@@ -55,22 +65,26 @@ Loop2:
 	btst.b #0,$A11100
 	bne	Loop2
 
-	lea ssgAttackProgram, A0 ; start of the test program to run
-	lea sampleDocProgram, A0 ; start of the test program to run
+	move.b #$40,$A10009 ; init joypad 1 by writing 0x40 to CTRL port for joypad 1
+
+	lea ssgAttackProgram, A1 ; start of the test program to run
+	lea sampleDocProgram, A1 ; start of the test program to run
+StartProgram:
+	move A1, A0
 
 RunProgram:
-	move.b (A0)+, D2	; D2 is the operation to perform
+	move.b (A0)+, D0	; D0 is the operation to perform
 
-	cmpi.b #0x00, D2
+	cmpi.b #0x00, D0
 	beq WritePort0
 
-	cmpi.b #0x01, D2
+	cmpi.b #0x01, D0
 	beq WritePort1
 
-	cmpi.b #0x10, D2
+	cmpi.b #0x10, D0
 	beq OutputSamples
 
-	cmpi.b #0xFF, D2
+	cmpi.b #0xFF, D0
 	beq EndProgram
 	
 	jmp	ErrorTrap			; unknown operation to perform
@@ -88,11 +102,15 @@ WritePort1:				; NOT IMPLEMENTED PROPERLY
 	jmp RunProgram
 
 OutputSamples:			; loop a lot
-	move.b (A0)+, D0	; need to load a word (16-bits) which is the number of samples to loop
-	move.b (A0)+, D1	; need to load a word (16-bits) which is the number of samples to loop
+	move.b (A0)+, D0	; bottom 8-bits
+	move.b (A0)+, D1	; top 8-bits
 OutputOneSample:
-	move.w #0x4FFF, D3 
+	move.w #0x4FFF, D3 ; this should be the number of cycles to output a single 44KHz sample
 OutputSample:
+	bsr UpdateGUI
+	cmpi.b #0x01, D7
+	beq StartProgram
+
 	subi.w #0x01, D3
 	bne OutputSample
 	subi.w #0x01, D0
@@ -100,7 +118,11 @@ OutputSample:
 	jmp RunProgram
 
 EndProgram:
-	jmp	ErrorTrap
+	bsr UpdateGUI
+	cmpi.b #0x01, D7
+	beq StartProgram
+
+	jmp	EndProgram
 
 WriteYM2612:
 	btst #7,$A04000
@@ -111,8 +133,44 @@ WriteYM2612Data:
 	bne WriteYM2612Data
 	move.b D1,$A04001
 	rts
+
+UpdateGUI:				; test for joypad input & draw screen text
+	move.b #$00, D7	; D7 is result of what to do from this function
+
+	move.b #$40, $A10003 ; ask for high-state on joypad 1 by writing 0x40 to DATA port for joypad 1
+	nop							; wait a couple of cycles
+	nop							; wait a couple of cycles
+	move.b $A10003, D5	; read joypad 1 : C-button, B-button + Right/Left/Down/Up
+
+	move.b #$00, $A10003 ; ask for low-state on joypad 1 by writing 0x00 to DATA port for joypad 1
+	nop							; wait a couple of cycles
+	nop							; wait a couple of cycles
+	move.b $A10003, D6	; read joypad 1 : Start-button + A-Button + Down/Up
+
+	; TODO-merge D6 & D5 into a single joypad 1 8-bit value: (D6 & 0x30 << 2) | D5
+
+	; debounce : (lastState ^ newState ) & newState -> true when button has been released
+	lea lastPadHigh, A2
+	move.b (A2), D4		; D4 is lastState, D6 is newState (1 = not pressed, 0 = pressed)
+	eor.b D6, D4		; D4 = lastState ^ newState
+	and.b D6, D4		; D4 = (lastState ^ newState) & newState
+
+	move.b D4, D2		; test for Start which is bit-5 -> 0x20
+	andi.b #$20, D2
+	beq NotStart		; test for start button not-pressed (0) -> pressed (1)
+
+	move.b #$01, D7	; start was pressed
+
+NotStart:
+
+SavePadState:
+	lea lastPadLow, A2
+	move.b D5, (A2)
+	lea lastPadHigh, A2
+	move.b D6, (A2)
+	rts
 	
-; Test Programs
+; Handy macros to make test programs easy to convert from C -> ASM
 
 LJ_TEST_PART_0 MACRO I, R, D, J
 	DB 0x00, R, D
@@ -130,8 +188,13 @@ LJ_TEST_FINISH MACRO I, R, D, J
 	DB 0xFF, R, D
 	ENDM
 
+; Test Programs
+
 ssgAttackProgram:
+ALIGN=2
+
 sampleDocProgram:
+ALIGN=2
 		LJ_TEST_PART_0, 0x22, 0x00,	/* LFO off */
 		LJ_TEST_PART_0, 0x27, 0x00,	/* Channel 3 mode normal */
 		LJ_TEST_PART_0, 0x28, 0x00,	/* All channels off */
@@ -180,3 +243,6 @@ sampleDocProgram:
 		LJ_TEST_OUTPUT, 0x30, 0x00,	/* OUTPUT SAMPLES */
 		LJ_TEST_FINISH, 0xFF, 0xFF,	/* END PROGRAM */
 
+; RAM Memory variables
+lastPadLow=0xFF0800
+lastPadHigh=0xFF0802
